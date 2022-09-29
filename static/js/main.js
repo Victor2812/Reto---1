d3df6b8bf6e250847c17a7a32a9263bd845e52e7
6e0d0a <gaizka.gg@outlook.es>
2022-09-29 13:34:00 +0200

    feat(matrix): generación de celdas

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,19 @@
 window.onload = function (){
-    // Código JS aquí dentro
+    const matrix_size = [5, 5];
+
+    // Crear matriz
+    function createMatrix(e) {
+        for (let x = 0; x < matrix_size[0]; x++) {
+            for (let y = 0; y < matrix_size[1]; y++) {
+                let cell = document.createElement('div');
+                cell.classList.add('cell');
+                cell.id = `r${x}c${y}`;
+                e.appendChild(cell);
+            }
+        }
+    }
+
+    // TEST de creación de la matriz
+    let e;
+    (e = document.querySelector('.content .matrix')) && createMatrix(e);
 };
