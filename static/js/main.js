1a1f80bb16e5fda80093ab6ce97d4a0b17a5bc46
Victor2812 <victor.ibanez@ikasle.egibide.org>
2022-09-29 22:54:32 +0200

    maq modo manual auto v

---
+++ static/js/main.js
@@ -0,0 +1,18 @@
+const auto = document.getElementById('auto');
+const manual = document.getElementById('manual');
+
+auto.addEventListener('click', function(e) {
+    auto.style.color = '#8D6448';
+    auto.style.textDecoration = 'underline';
+
+    manual.style.color = '#473123';
+    manual.style.textDecoration = 'none';
+});
+
+manual.addEventListener('click', function(e) {
+    auto.style.color = '#473123';
+    auto.style.textDecoration = 'none';
+
+    manual.style.color = '#8D6448';
+    manual.style.textDecoration = 'underline';
+});
