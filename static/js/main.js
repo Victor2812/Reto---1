1bf2405b39ed09cbbe73e529b11c90dad5e7f516
6e0d0a <gaizka.gg@outlook.es>
2022-10-03 13:00:15 +0200

    fix(header): ahora el botón de encendido y apagado funciona bien

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,11 @@
-window.onload = function (){
+window.onload = function() {
     const matrix_size = [5, 5];
 
-    const onoff = document.querySelector('#onoff');
-    onoff.addEventListener('click', function(e){
-        onoff.classList.toggle("off");
-        onoff.classList.toggle("on");
-    });
+    const machine = new ConsoleMachine();
+    const controller = new MachienController(machine);
 
-    const auto = document.getElementById('auto');
-    const manual = document.getElementById('manual');
-
-    auto.addEventListener('click', function(e) {
-        auto.style.color = '#8D6448';
-        auto.style.textDecoration = 'underline';
-
-        manual.style.color = '#473123';
-        manual.style.textDecoration = 'none';
-    });
-
-    manual.addEventListener('click', function(e) {
-        auto.style.color = '#473123';
-        auto.style.textDecoration = 'none';
-
-        manual.style.color = '#8D6448';
-        manual.style.textDecoration = 'underline';
-    });
-    
+    let content = document.querySelector('.content-screen .content');
+    let buttons = document.querySelector('.content-screen .buttons');
 
     // Crear matriz
     function createMatrix(e) {
@@ -39,6 +19,9 @@ window.onload = function (){
         }
     }
 
-    // TEST de creación de la matriz
-    let e; (e = document.querySelector('.content .matrix')) && createMatrix(e);
+    document.querySelector('#onoff')
+        .addEventListener('click', function() {
+            controller.changeMode(!controller.getCurrentMode());
+            this.classList.toggle('active');
+        });
 };
