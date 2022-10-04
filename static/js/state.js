00b5a3f911fcc43079e41b306d40e286d53831ce
6e0d0a <gaizka.gg@outlook.es>
2022-10-04 12:17:57 +0200

    fix(state): ahora los estados son asíncronos

--- static/js/state.js
+++ static/js/state.js
@@ -2,33 +2,69 @@ class BaseMachine {
     #state = false;
     #mode = true;  // false = auto, true = manual
 
-    changeState(newState, callback) {
+    async changeState(newState) {
         this.#state = newState;
     }
 
-    getState() {
+    async getState() {
         return this.#state;
     }
 
-    changeMode(newMode, callback) {
+    async changeMode(newMode) {
         this.#mode = newMode;
     }
 
-    getMode() {
+    async getMode() {
         return this.#mode;
     }
 }
 
 class ConsoleMachine extends BaseMachine {
-    changeState(newState, callback) {
+    async changeState(newState) {
         console.log('changeState => ' + newState);
         super.changeState(newState);
-        callback(this.getState());
     }
 
-    changeMode(newMode, callback) {
+    async changeMode(newMode) {
         console.log('changeMode => ' + newMode);
         super.changeMode(newMode);
-        callback(this.getMode());
+    }
+}
+
+class StateManager {
+    constructor(machine) {
+        this.machine = machine;
+    }
+
+    /**
+     * Cambia el estado en el que se encuentra la máquina, de encendido a apagado y viceversa
+     */
+    async toggleState() {
+        let newState = !await this.machine.getState();
+        await this.machine.changeState(newState);
+    }
+
+    /**
+     * Obtiene el estado en el que se encuentra la máquina, encendida o apagada
+     * @returns True o False
+     */
+    async getState() {
+        return await this.machine.getState();
+    }
+
+    /**
+     * Cambia el modo de la máquina
+     * @param {Boolean} mode Automático (false) o manual (true)
+     */
+    async setMode(mode) {
+        await this.machine.changeMode(mode);
+    }
+
+    /**
+     * Obtiene el modo en el que la máquina está funcionando
+     * @returns Manual (true) o automático (False)
+     */
+    async getMode() {
+        return await this.machine.getMode();
     }
 }
