4b3a5e6a9d9660411b09d922ec86ce52c6649561
Victor2812 <victor.ibanez@ikasle.egibide.org>
2022-10-07 11:44:15 +0200

    funciones conexion plc

--- static/js/vm-machine.js
+++ static/js/vm-machine.js
@@ -9,82 +9,58 @@ class VMMachine extends BaseMachine {
         return variables;
     }
 
-    /**
-     * Cambio estado on/off via fetch -> post json
-     */
-    //falta función que llame a los fetch:
-    /*
-    let state = false;
-
-    fucntion changeStateVar(){
-        state = !state;
-        if(state == true){
-            changeSateOn();
-        } else {
-            changeStateOff();
-        }
-    }
-    */ 
-    async changeStateOn(){
-        let response = fetch(".../variables/variables.html", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json;"
-                },
-                body: JSON.stringify({
-                    "martxa": true
-                })
-            });
-        let estado = await response.json();
-        return estado;
-    }
-    async changeStateOff(){
-        let response = fetch(".../variables/variables.html", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json;"
-                },
-                body: JSON.stringify({
-                    "martxa": false
-                })
-            });
-        let estado = await response.json();
-        return estado;
-    }
-    async changeModel() {
-        
-    }
-    /**
-     * Genera formularios invisibles para cambiar de estado el modo dentro del PLC, necesita llmada desde el botonx
-     */
-    async setModeAuto() {
-        var formAuto = document.createElement("form");
-        formAuto.name = "Auto";
-        formAuto.method = "POST";
-        contenedor.appendChild(formAuto);
-
-        var mode = document.createElement("input");
-        mode.type = "hidden";
-        mode.name = '"wonka".modo';
-        mode.value = 0;
-        formAuto.appendChild(mode);
-
-        formAuto.submit();
+    async setVariableVM(name, value) {
+        let response = await fetch(".../variables/variables.html", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json;"
+            },
+            body: JSON.stringify({
+                nombre: valor
+            })
+        });
+        return response;
     }
 
-    async setModeManual() {
-        var formManual = document.createElement("form");
-        formManual.name = "Manual";
-        formManual.method = "POST";
-        contenedor.appendChild(formManual);
-
-        var mode = document.createElement("input");
-        mode.type = "hidden";
-        mode.name = '"wonka".modo';
-        mode.value = 1;
-        formManual.appendChild(mode);
-
-        formManual.submit();
+    async changeState(newState){
+        let response = await this.setVariableVM('martxa', newState);
+        return await response.json();
     }
 
+    async getState() {
+        return await this.getVariablesVM()['Martxa'];
+    }
+
+    async changeMode(newMode) {
+        let response = await this.setVariableVM('modo', newMode);
+        return await response.json();
+    }
+
+    async getMode() {
+        return await this.getVariablesVM()['Modo'];
+    }
+
+    async setMatrixPos(x, y, value) {
+        console.log('testing');
+    }
+
+    async getMatrixPos(x, y) {
+        return CHOC_CONLECHE;
+    }
+
+    async getMatrix() {
+    return [[1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1]];
+    }
+
+    async getNextChocolate() {
+        return 1;
+    }
+
+    async wasteCurrentChocolate() {
+        console.log('testing');
+    }
 }
